d57c4f7057148b4d6ee2e03297574bcc7bef996c
Yavuz <yavuz.sonmez.pro@gmail.com>
2020-07-03 04:13:18 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  constructor(props) 
+  {
+    super(props);
+    this.state = 
+     {
+        quote: "",
+        author: ""
+      };
+   }
+
+
+  componentDidMount() {
+let random = [Math.floor(Math.random() * (5420 - 0 + 1)) + 0];
+    fetch("https://raw.githubusercontent.com/JamesFT/Database-Quotes-JSON/master/quotes.json")
+      .then(res => res.json())
+      .then( (result) => {
+ this.setState({
+    quote: result[random].quoteText,
+   author: result[random].quoteAuthor
+          });
+        },
+        (error) => {
+          this.setState({
+            quote: "error",
+            author: "error"
+          });
+        }
+      )
+  }
+    
+  
+  render() {
+    return (
+    <div id="quote-box">
+        <h2 id="text">" {this.state.quote} "</h2>
+        <p id="author">{this.state.author}</p>
+               <div class="text-center"> <button type="button" class="btn btn-primary" id="new-quote" onClick={this.componentDidMount.bind(this)}>New Quote</button>
+     <button type="button" class="btn btn-primary">   <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a></button>
+                 </div>
+                 <p id="por"><a id="portofolio" href="https://yavuzsonmez.com/portofolio">Return to my Portofolio</a></p>
+     </div>
+    );
+  }
+  }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
